e454dbfe892d48b350cb49099ffe72e43d786078
amslimaa <social.amos@gmail.com>
2022-08-20 19:29:44 -0300

    #48

---
+++ backend/src/controllers/Parking/CreateParkingController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { prismaClient } from '../../database/prismaClient';
+
+export class CreateParkingController {
+  async handle(req: Request, res: Response) {
+    const { name, latitude, longitude } = req.body;
+   
+    const createdParking = await prismaClient.parking.create({data:{name, latitude, longitude}});
+    res.json(createdParking);
+  }
+}
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from "express";
 import { Router } from "express";
-
 const routes = Router()
+
+import {CreateParkingController} from "./controllers/Parking/CreateParkingController";
+import { GetParkingController } from "./controllers/Parking/GetParkingController";
+
 routes.get("/",(req: Request, res: Response) => {
-    return res.json({ message: "Hello World" })
+    return res.json({ message: "Iparking API, for more info please access https://github.com/ES2-UFPI/iparking" })
 })
+
+routes.post("/parking", new CreateParkingController().handle)
+routes.get("/parking", new GetParkingController().handle)
+    
 export  {routes};
