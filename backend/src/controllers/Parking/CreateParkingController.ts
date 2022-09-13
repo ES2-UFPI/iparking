48250d9358b96bb8a36f090d6504806026fbb2fc
amslimaa <social.amos@gmail.com>
2022-09-13 00:36:08 -0300

    add parking spaces

--- backend/src/controllers/Parking/CreateParkingController.ts
+++ backend/src/controllers/Parking/CreateParkingController.ts
@@ -3,9 +3,9 @@ import { prismaClient } from '../../database/prismaClient';
 
 export class CreateParkingController {
   async handle(req: Request, res: Response) {
-    const { image_url, mail, phone, name, first_price, price,latitude, longitude } = req.body;
+    const { image_url, mail, phone, name, parking_spaces, first_price, price,latitude, longitude } = req.body;
 
-    const parking = await prismaClient.parking.create({data: {image_url, mail, phone, name, first_price, price,latitude, longitude, }});
+    const parking = await prismaClient.parking.create({data: {image_url, mail, phone, name, parking_spaces, first_price, price,latitude, longitude, }});
     return res.json(parking)
   }
 }
