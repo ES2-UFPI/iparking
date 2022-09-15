2efd3152ca736b52fc6a4b74be08a4479d5e856e
amslimaa <social.amos@gmail.com>
2022-09-15 06:18:48 -0300

    Create WebApp iParking #74

---
+++ web/src/styled/Home.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 1000px;
+  height: 1000px;
+`;
+export const Map = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 500px;
+  height: 600px;
+`;
