af4179a76b735bac70b17f69bcffa1e5c694da04
amslimaa <social.amos@gmail.com>
2022-09-15 06:38:18 -0300

    add map view #74

--- web/src/pages/App.tsx
+++ web/src/pages/App.tsx
@@ -1,7 +1,33 @@
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+
+import './styles.css';
 
 function App() {
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyDCS-Fp2TKI2gLFOWH8AJhHRvDwxpJdNbU"
+  })
+
   return (
-  <h1>ok</h1>
+    <div className='map'>
+      {isLoaded ? (
+        <GoogleMap
+          mapContainerStyle={{width: '100%', height: '100%'}}
+          center={{
+            lat:-5.0562191,
+            lng: -42.7920532
+          }}
+          zoom={15}
+          
+        >
+          { /* Child components, such as markers, info windows, etc. */}
+          <></>
+        </GoogleMap>
+      ) : <></>
+      }
+    </div>
   )
 }
 
--- web/src/styled/Home.ts
+++ web/src/styled/Home.ts
@@ -1,11 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 1000px;
-  height: 1000px;
+  width: 100%;
+  height: 100%;
 `;
 export const Map = styled.div`
   display: flex;
