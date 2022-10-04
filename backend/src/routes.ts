261f619b41af06de7e15e3bcec310799e1cac3eb
Amós Lima <social.amos@gmail.com>
2022-10-04 12:20:58 +0000

    create user controller #20

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -25,5 +25,7 @@ routes.get("/ticket", new GetTicketController().handle);
 routes.get("/ticket/:id", new GetTicketController().handle);
 routes.delete("/ticket/:id", new DeleteTicketController().handle);
 routes.put("/ticket/:id", new CloseTicketController().handle);
-    
+
+
+
 export  {routes};
