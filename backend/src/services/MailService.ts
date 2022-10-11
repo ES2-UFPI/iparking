553bdb63c14427aff7b3473cc11307d1a24949c0
Amós Lima <social.amos@gmail.com>
2022-10-11 22:34:55 +0000

    config send mail

---
+++ backend/src/services/MailService.ts
@@ -0,0 +1,13 @@
+import * as nodemailer from "nodemailer";
+
+let transporter = nodemailer.createTransport( {
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: false,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+    }
+})
+
+export { transporter }
